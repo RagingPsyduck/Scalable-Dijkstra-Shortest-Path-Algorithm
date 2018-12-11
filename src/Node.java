8fb1e84137f25766b4c29e8ef139ae00d73993d0
Teng Yang <yangteng0813@gmail.com>
2018-12-11 17:54:24 -0500

    Removed code

--- src/Node.java
+++ src/Node.java
@@ -3,14 +3,9 @@ import java.util.*;
 public class Node {
     int id;
     List<Edge> edges;
-//    List<Integer> costs;
-//    List<Node> connectedNodes;
-
 
     Node(int x) {
         id = x;
         edges = new ArrayList<>();
-//        costs = new ArrayList<>();
-//        connectedNodes = new ArrayList<>();
     }
 }
