e7f4de0e3772b6bea22e0f53731e25439ea36a08
Teng Yang <yangteng0813@gmail.com>
2018-12-11 21:27:18 -0500

    Reformat the code

--- src/TwoModel.java
+++ src/TwoModel.java
@@ -1,4 +1,4 @@
-public class TwoModel {
+public class TwoModel extends Thread{
     public void generatePath(Node[][] nodes) {
         System.out.println("--------------------------");
         Util util = new Util();
@@ -24,28 +24,28 @@ public class TwoModel {
             long totalTime = endTime - startTime;
 
             System.out.println("Process one Time " + totalTime / 1000);
-            DijkstraAlgorithm pathPlan2 = new DijkstraAlgorithm();
-            pathPlan2.initPathPlanning(nodes);
-
-            startTime = System.nanoTime();
-            long totalCleanUpTime = 0;
-            int res = Integer.MAX_VALUE;
-            synchronized (pathPlan1) {
-                int startX = nodes.length / 2;
-                for (int i = 0; i < costParser.length; i++) {
-                    //long  tempStartTime = System.nanoTime();
-                    pathPlan2.cleanDistanceMap();
-                    pathPlan2.initPathPlanning(nodes);
-                    //long  tempEndTime = System.nanoTime();
-                    //totalCleanUpTime += tempEndTime - tempStartTime;
-                    int cost = pathPlan2.bfs(nodes, startX, i, nodes.length - 1, nodes[0].length - 1).cost;
-                    res = Math.min(res, cost + costParser[i]);
-                }
-            }
-
-            endTime = System.nanoTime();
-            totalTime = endTime - startTime - totalCleanUpTime;
-            System.out.println("Process Two Time " + totalTime / 1000 + " cost " + res);
         }
+
+        DijkstraAlgorithm pathPlan2 = new DijkstraAlgorithm();
+        pathPlan2.initPathPlanning(nodes);
+        startTime = System.nanoTime();
+        long totalCleanUpTime = 0;
+        int res = Integer.MAX_VALUE;
+        synchronized (pathPlan1) {
+            int startX = nodes.length / 2;
+            for (int i = 0; i < costParser.length; i++) {
+                //long  tempStartTime = System.nanoTime();
+                pathPlan2.cleanDistanceMap();
+                pathPlan2.initPathPlanning(nodes);
+                //long  tempEndTime = System.nanoTime();
+                //totalCleanUpTime += tempEndTime - tempStartTime;
+                int cost = pathPlan2.bfs(nodes, startX, i, nodes.length - 1, nodes[0].length - 1).cost;
+                res = Math.min(res, cost + costParser[i]);
+            }
+        }
+
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime - totalCleanUpTime;
+        System.out.println("Process Two Time " + totalTime / 1000 + " cost " + res);
     }
 }
